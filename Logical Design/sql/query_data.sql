4149ac77f8a20530c5ba37aad3be5b404cfe17de
youssef <youssef@benhammouda.ma>
2025-10-10 20:27:37 +0100

    Refactor SQL query to improve readability and structure for patient appointment retrieval

--- Logical Design/sql/query_data.sql
+++ Logical Design/sql/query_data.sql
@@ -1,10 +1,26 @@
-SELECT * FROM Patient WHERE IID IN (
-    SELECT IID FROM Clinical_Activity WHERE CAID IN (
-        SELECT CAID FROM Appointment WHERE Status = 'Scheduled'
-    )
-    AND DEP_ID IN (
-        SELECT DEP_ID FROM Department WHERE HID IN (
-            SELECT HID FROM Hospital WHERE City = 'BenGuerir'
-        )
-    )
-);
+SELECT 
+    p.Name,
+    p.CIN,
+    p.Birth,
+    p.Blood_group,
+    p.Sex,
+    p.Phone,
+    a.Reason,
+    a.Status,
+    h.Name AS Hospital_Name,
+    h.City,
+    d.Name AS Department_Name,
+    d.Specialty
+FROM 
+    Patient p,
+    Clinical_Activity c,
+    Appointment a,
+    Department d,
+    Hospital h
+WHERE 
+    p.IID = c.IID
+    AND c.CAID = a.CAID
+    AND c.DEP_ID = d.DEP_ID
+    AND d.HID = h.HID
+    AND h.City = 'benguerir'
+    AND a.Status = 'Scheduled';
