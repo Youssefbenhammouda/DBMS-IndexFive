75f3866091332d277c87b8b80d3286fa25166e27
BIAR ADAM <biaradam36@gmail.com>
2025-11-16 16:13:23 +0100

    Add update for Clinical_Activity occurred_at field
    Added an update statement to modify the occurred_at field for a specific clinical activity.

--- Logical Design/sql/insert_data.sql
+++ Logical Design/sql/insert_data.sql
@@ -241,9 +241,13 @@ UPDATE Hospital
 SET Region = 'Marrakech-Safi'
 WHERE HID=1;
 
-
+UPDATE Clinical_Activity
+SET occurred_at =Now()- INTERVAL 3 DAY
+WHERE CAID=4;
 
 
 DELETE FROM Appointment
 WHERE Status = 'Cancelled'
 LIMIT 1;
+
+
