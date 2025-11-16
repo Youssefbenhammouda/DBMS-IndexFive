164b6d58505a886798632a174072b71073b1682e
At1asBe <yahiabelefquih1@gmail.com>
2025-11-16 17:58:38 +0100

    Update queries.sql
    queries queries queries queries queries queries queries queries queries queries queries queries queries queries queries queries

--- Logical Design/sql/lab05/deliverables/queries.sql
+++ Logical Design/sql/lab05/deliverables/queries.sql
@@ -141,6 +141,42 @@ WHERE NOT EXISTS (
       )
   );
 
+-- Query 15
+WITH HospitalAvg AS (
+    SELECT 
+        s.HID AS hospital_id, 
+        m.Class AS drug_class,
+        AVG(s.Unit_Price) AS avg_price
+    FROM Stock s
+    JOIN Medication m ON s.DrugID = m.DrugID
+    GROUP BY s.HID, m.Class
+), 
+CityAvg AS (
+    SELECT 
+        h.City AS city_name,
+        m.Class AS drug_class,
+        AVG(s.Unit_Price) AS city_avg
+    FROM Stock s
+    JOIN Medication m ON s.DrugID = m.DrugID
+    JOIN Hospital h ON s.HID = h.HID
+    GROUP BY h.City, m.Class
+)
+SELECT 
+    ha.hospital_id,
+    ha.drug_class,
+    ha.avg_price,
+    CASE 
+        WHEN ha.avg_price > ca.city_avg THEN 'Above'
+        ELSE 'Not Above'
+    END AS price_flag
+FROM HospitalAvg ha
+JOIN Hospital h ON ha.hospital_id = h.HID
+JOIN CityAvg ca 
+    ON h.City = ca.city_name 
+   AND ha.drug_class = ca.drug_class;
+
+
+
 -- Query 16
 select P.IID,
   MIN(C.occurred_at)
@@ -164,6 +200,18 @@ GROUP BY P.IID,
   P.Name
 HAVING count1 >= 2
   AND max1 >= CURRENT_DATE() - INTERVAL 14 DAY;
+
+-- Query 18
+
+SELECT H.HID, H.Name, H.City, COUNT(*) AS Completed
+FROM Hospital H
+JOIN Department D ON D.HID = H.HID
+JOIN ClinicalActivity CA ON CA.DEP_ID = D.DEP_ID
+JOIN Appointment A ON A.CAID = CA.CAID
+WHERE A.status = 'Completed'
+  AND CA.Date >= CURRENT_DATE() - INTERVAL 90 DAY
+GROUP BY H.HID, H.Name, H.City
+ORDER BY H.City, Completed DESC;
 
 -- Query 19: Within each city return medications whose hospital prices show a spread greater than thirty percent between minimum and maximum.
 select t.city,
