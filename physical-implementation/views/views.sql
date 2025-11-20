19ff8ec7a1dd08eed8643eb735d7409d568aebee
AD?M <ajerouassiadam@gmail.com>
2025-11-20 12:27:50 +0100

    update

--- physical-implementation/views/views.sql
+++ physical-implementation/views/views.sql
@@ -1,38 +1,35 @@
 -- view 1
 CREATE OR REPLACE VIEW UpcomingByHospital AS
 SELECT 
-    H.`Name` as HospitalName,
-    C.`Date` as ApptDate,
-    count(C.`CAID`)
-FROM `Appointment` A
-    JOIN `ClinicalActivity` C ON C.`CAID` = A.`CAID`
-    JOIN `Department` D ON D.`DEP_ID` = C.`DEP_ID`
-    JOIN `Hospital` H ON H.`HID` = D.`HID`
-WHERE A.`Status` = 'Scheduled'
+    H.Name AS HospitalName,
+    CA.Date AS ApptDate,
+    count(CA.CAID)
+FROM Appointment A
+    JOIN ClinicalActivity CA ON CA.CAID = A.CAID
+    JOIN Department D ON D.DEP_ID = CA.DEP_ID
+    JOIN Hospital H ON H.HID = D.HID
+WHERE A.Status = Scheduled
     AND (
-        C.`Date` BETWEEN CURRENT_DATE() AND DATE_ADD(CURRENT_DATE(), INTERVAL 14 DAY)
+        CA.Date BETWEEN CURRENT_DATE() AND DATE_ADD(CURRENT_DATE(), INTERVAL 14 DAY)
     )
-GROUP BY H.`HID`,C.`Date`;
+GROUP BY H.HID,CA.Date;
 
 
 -- view 2
 CREATE OR REPLACE VIEW DrugPricingSummary AS
 SELECT 
-    S.HID as HID,
-    H.Name as HospitalName,
-    S.MID as MID,
-    M.Name as MedicationName,
-    AVG(S.UnitPrice) as AvgUnitPrice,
-    MIN(S.UnitPrice) as MinUnitPrice,
-    MAX(S.UnitPrice) as MaxUnitPrice,
-    MAX(S.StockTimestamp) as LastStockTimestamp
+    S.HID AS HID,
+    H.Name AS HospitalName,
+    S.MID AS MID,
+    M.Name AS MedicationName,
+    AVG(S.UnitPrice) AS AvgUnitPrice,
+    MIN(S.UnitPrice) AS MinUnitPrice,
+    MAX(S.UnitPrice) AS MaxUnitPrice,
+    MAX(S.StockTimestamp) AS LastStockTimestamp
 FROM Stock S
     JOIN Hospital H ON H.HID = S.HID
-    JOIN Medication M ON S.MID = M.MID
-GROUP BY S.HID,
-    H.Name,
-    S.MID,
-    M.Name;
+    JOIN Medication M ON M.MID = S.MID
+GROUP BY S.HID, H.Name, S.MID, M.Name;
 
 
 -- view 3
@@ -45,7 +42,7 @@ SELECT
     COUNT(CASE WHEN A.Status = 'Completed' THEN 1 END) AS CompletedCount,
     COUNT(CASE WHEN A.Status = 'Cancelled' THEN 1 END) AS CancelledCount
 FROM staff S
-    JOIN ClinicalActivity CA ON S.STAFF_ID = CA.STAFF_ID
-    JOIN Appointment A ON CA.CAID = A.CAID
+    JOIN ClinicalActivity CA ON CA.STAFF_ID = S.STAFF_ID
+    JOIN Appointment A ON A.CAID = CA.CAID
 WHERE CA.DATE BETWEEN CURRENT_DATE() AND DATE_ADD(CURRENT_DATE(), INTERVAL 30 DAY)
 GROUP BY S.STAFF_ID, S.Name;
