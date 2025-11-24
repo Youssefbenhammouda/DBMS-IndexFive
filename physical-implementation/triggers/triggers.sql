71ddc789488bf1b124b1814211af364d301c81b0
youssef <youssef@benhammouda.ma>
2025-11-24 21:44:04 +0100

    Add function to recalculate Expense.Total and update trigger for Includes

--- physical-implementation/triggers/triggers.sql
+++ physical-implementation/triggers/triggers.sql
@@ -45,6 +45,84 @@ BEGIN
 END $$
 
 
+-- 2. Recompute Expense.Total when prescription lines change.
+
+DROP FUNCTION IF EXISTS calculate_expense $$
+CREATE FUNCTION calculate_expense(CAID INT) RETURNS DECIMAL(10,2) READS SQL DATA
+BEGIN
+	DECLARE null_cnt int default 0;
+    DECLARE sum_prices DECIMAL(10,2) default 0;
+    SELECT count(*) INTO null_cnt
+FROM (
+        SELECT S.*, ROW_NUMBER() OVER (
+                PARTITION BY
+                    S.`MID`
+                ORDER BY S.`StockTimestamp` DESC
+            ) as rn_m
+        from
+            `ClinicalActivity` C
+            JOIN `Prescription` P ON P.`CAID` = C.`CAID`
+            JOIN `Includes` I ON I.`PID` = P.`PID`
+            JOIN `Department` D ON D.`DEP_ID` = C.`DEP_ID`
+            JOIN `Hospital` H ON H.`HID` = D.`HID`
+            LEFT JOIN `Stock` S ON (
+                S.`MID` = I.`MID`
+                AND S.`HID` = H.`HID`
+            )
+        WHERE
+            C.`CAID` = CAID
+    ) X
+WHERE X.rn_m=1 and UnitPrice IS null;
+
+	IF null_cnt > 0
+    THEN
+    SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Missing unit price.';
+    END IF;
+    
+    SELECT SUM(`UnitPrice`) INTO sum_prices
+FROM (
+        SELECT S.*, ROW_NUMBER() OVER (
+                PARTITION BY
+                    S.`MID`
+                ORDER BY S.`StockTimestamp` DESC
+            ) as rn_m
+        from
+            `ClinicalActivity` C
+            JOIN `Prescription` P ON P.`CAID` = C.`CAID`
+            JOIN `Includes` I ON I.`PID` = P.`PID`
+            JOIN `Department` D ON D.`DEP_ID` = C.`DEP_ID`
+            JOIN `Hospital` H ON H.`HID` = D.`HID`
+            LEFT JOIN `Stock` S ON (
+                S.`MID` = I.`MID`
+                AND S.`HID` = H.`HID`
+            )
+        WHERE
+            C.`CAID` = CAID
+    ) X
+WHERE X.rn_m=1 ;
+
+return sum_prices;
+        
+
+
+END $$
+
+
+DROP TRIGGER IF EXISTS trg_iclude_after_update $$
+CREATE  TRIGGER trg_iclude_after_update
+AFTER UPDATE ON Includes
+FOR EACH ROW
+BEGIN 
+	declare new_total decimal(10,2);
+    declare caid int;
+    declare expid int;
+    SELECT P.`CAID` INTO caid   FROM `Prescription` P WHERE P.`PID`=NEW.PID;
+    SELECT E.`ExpID` into expid from `Expense` E WHERE E.`CAID`=caid;
+    SET new_total = calculate_expense( caid);
+    UPDATE `Expense` SET `Total`=new_total WHERE `ExpID`=expid;
+END $$
+
 	
 -- 3.Prevent negative or inconsistent stock.
 
